76764bd85d99b32d199f7ffb2b0a6c88683046fb
NicolasKorzusehec <nkorzusehec@gmail.com>
2022-07-05 02:38:20 -0300

    Reformulando los filtros y la ejecucion hacia un objeto que contenga las desiciones

--- filtrar.py
+++ filtrar.py
@@ -2,21 +2,21 @@ import csv
 
 # Esta funcion desacoplada me permite crear los filtros de forma indepediente en el script `filtrar.py` bajo la premisa de incluir el arreglo que contiene los filtros como parametro de las mismas, logrando asi mejorar sus especificidades facilmente. Inclusive el usuario podria decidir que filtros aplicar.
 # !!! Se debe respetar la posicion  de la funcion 'nombreArchivo()' y 'salida()' para no romper el codigo en su ejecucion posteriormente!!!
-def filtrosCsv(arreglo):
+def filtrosCsv(obj):
     print ("""
 Acontinuacion se le solicitara informacion para filtrar un archivo csv y de esa forma visualizar toda la informacion de los cheques emitidos o depositados por el cliente en cuestion.
 """)
     # Dispondra de la informacion en el siguiente orden.
     # Nombre archivo, salida buscada, DNI del cliente, tipo de cheque, estado del cheque, fecha origen, fecha pago.
-    nombreArchivo(arreglo)
-    salida(arreglo)
-    ingresoDni(arreglo)
-    tipoCheque(arreglo)
-    estadoCheque(arreglo)
-    fechaOrigen(arreglo)
-    fechaPago(arreglo)
+    nombreArchivo(obj)
+    salida(obj)
+    ingresoDni(obj)
+    tipoCheque(obj)
+    estadoCheque(obj)
+    fechaOrigen(obj)
+    fechaPago(obj)
 
-def nombreArchivo(array):
+def nombreArchivo(obj):
     estado = False
     print("""Ingrese el nombre del archivo a filtrar:
     Ejemplo: test.csv (disponible)
@@ -27,14 +27,14 @@ def nombreArchivo(array):
             nombreArchivo = input()
             file = open("src/"+nombreArchivo, "r")
             file.close()
-            array.append ("src/"+nombreArchivo)
+            obj["archivo"] ="src/"+nombreArchivo
             estado = True
         except FileNotFoundError:
             print ("""
             No se ha encontrado el archivo. Vuelve a intentar.""")
             continue
 
-def salida(array):
+def salida(obj):
     estado = False
     print ("""
 Como quisiera visualizar el resultado?
@@ -46,50 +46,53 @@ Como quisiera visualizar el resultado?
         salida = input()
         if salida.isdigit():
             if int(salida) == 1:
-                array.append("pantalla")
+                obj["salida"] = "pantalla"
                 estado = True
             elif int(salida) == 2:
-                array.append ("archivo")
+                obj["salida"] = "archivo"
                 estado = True
             else:
                 print("Esa opcion no es correcta.")
         else:
             print("Esa opcion no es correcta.")
 
-def ingresoDni(array):
+def ingresoDni(obj):
     estado = False
     print ("Ingrese el DNI del cliente: ")
     while estado != True:
         dni = input ()
         if dni.isdigit():
-            array.append (int (dni))
+            obj["DNI"] = int (dni)
             estado = True
         else:
             print ("Solo se aceptan enteros sin puntos, comas, ni espacios")
 
-def tipoCheque(array):
+def tipoCheque(obj):
     estado = False
     print ("""
 El cheque fue:
 >>> Ingrese el numero de su respuesta
     1. Emitido
     2. Depositado
+    3. No requiero especificar
     """)
     while estado != True:
         tipo = input()
         if tipo.isdigit():
             if int(tipo) == 1:
-                array.append("Emitido")
+                obj["Tipo"] = "Emitido"
                 estado = True
             elif int(tipo) == 2:
-                array.append ("Depositado")
+                obj["Tipo"] = "Depositado"
                 estado = True
+            elif int(tipo) == 3:
+                return
             else:
                 print("Esa opcion no es correcta.")
         else:
             print("Esa opcion no es correcta.")
 
-def estadoCheque(array):
+def estadoCheque(obj):
     estado = False
     print ("""
 Cual es el estado del cheque:
@@ -97,29 +100,32 @@ Cual es el estado del cheque:
     1. Pendiente
     2. Aprobado
     3. Rechazado
+    4. No requiero especificar
     """)
     while estado != True:
         tipo = input()
         if tipo.isdigit():
             if int(tipo) == 1:
-                array.append("Pendiente")
+                obj["Estado"] = "Pendiente"
                 estado = True
             elif int(tipo) == 2:
-                array.append ("Aprobado")
+                obj["Estado"] = "Aprobado"
                 estado = True
             elif int(tipo) == 3:
-                array.append ("Rechazado")
+                obj["Estado"] = "Rechazado"
                 estado = True
+            elif int(tipo) == 4:
+                return
             else:
                 print("Esa opcion no es correcta.")
         else:
             print("Esa opcion no es correcta.")
 
-def fechaOrigen(array):
+def fechaOrigen(obj):
     print("Pendiente programar fechaOrigen")
-    array.append("Pendiente")
+    obj["FechaOrigen"] ="Pendiente"
 
-def fechaPago(array):
+def fechaPago(obj):
     print("Pendiente programar fechaPago")
-    array.append("Pendiente")
+    obj["Fecha"] ="Pendiente"
 
--- listado_cheques.py
+++ listado_cheques.py
@@ -5,12 +5,12 @@ from ejecutar import * # Todas las funciones usadas por el programa para ejecuta
 #Desde la obtencion del archivo csv y su conversion a un objeto utilizable, hasta su posterior salida. Depende de las decisiones tomadas por el usuario.
 
 # Defino variables.
-filtros = [] # Decisiones
+desiciones = {} # Decisiones
 archivoCheques = [] # En bruto
-objetoDatos=[] # Objeto utilizable derivado del archivoCheques
+objDatos=[] # Objeto utilizable derivado del archivoCheques
 resultados = [] # Objeto ya filtrado y dispuesto para su salida
 
-
+# Filtra el objeto creado a partir del csv con las deciciones del usuario de la funcion 'filtrarCsv()', luego incluye esos resultados en el objeto resultados.
 def filtradoDicc(diccionario, filtros, resultados):
     print("hi")
 
@@ -20,19 +20,19 @@ if __name__ == '__main__':
     El programa ofrece una prueba con el archivo "test.csv" incluido""")
     
     # Crea un array con todos los filtros deseados por el usuario.
-    filtrosCsv(filtros)
-    print(filtros)
+    filtrosCsv(desiciones)
+    print(desiciones)
 
     # Recibe el nombre del archivo desde el primer elemento del array 'filtros' y lo asigna a la variable 'archivoCheques'  para poder trabajar sobre el.
-    readFile(archivoCheques, filtros)
+    readFile(archivoCheques, desiciones)
 
     # Itera en cada fila util del archivo a filtrar creando un objeto que se incluye en la lista 'objetoDatos' para trabajarla facilmente posteriormente.
-    crearDicc(archivoCheques, objetoDatos)
-    for obj in objetoDatos:
+    crearDicc(archivoCheques, objDatos)
+    for obj in objDatos:
         print (obj)
 
     # Filtra el objeto creado a partir del csv con las deciciones del usuario de la funcion 'filtrarCsv()', luego incluye esos resultados en el objeto resultados.
-    filtradoDicc(objetoDatos, filtros, resultados)
+    filtradoDicc(objDatos, desiciones, resultados)
 
     # Recibe la decision del usuario sobre como presentar los resultados e invoca la funcion correspondiente a la misma.
-    output(resultados, filtros)
+    output(resultados, desiciones)
